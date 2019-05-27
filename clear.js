b355bbeafd55e1ef002a8bce2cb4bbda84b2dbba
河边的草帽 <helloxiangsheng@163.com>
2019-05-27 23:52:48 +0800

    Update clear.js

--- clear.js
+++ clear.js
@@ -1,7 +1,9 @@
-if(!pm.environment.has('localStore')){
-    pm.environment.set('localStore', '{}');
-}
+(function clera(){
+    if(!pm.environment.has('localStore')){
+        pm.environment.set('localStore', '{}');
+    }
 
-let localStore = JSON.parse(pm.environment.get('localStore'));
-Object.keys(localStore).map(key => pm.environment.unset(key));
-pm.environment.unset('localStore');
+    let localStore = JSON.parse(pm.environment.get('localStore'));
+    Object.keys(localStore).map(key => pm.environment.unset(key));
+    pm.environment.unset('localStore');
+})();
