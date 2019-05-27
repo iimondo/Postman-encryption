e0bdbdd7eb4a6ab2dc826760784c8483b1c31827
河边的草帽 <helloxiangsheng@163.com>
2019-05-27 23:22:11 +0800

    Update encryption.js

--- encryption.js
+++ encryption.js
@@ -1,98 +1,154 @@
-// ------ 通用方法 ------
+// ------------ 通用方法 ------------
 // 提取{{}}中内容
 function getBracketStr(text) {
-    let result = ''
-    let regex = /\{\{(.+?)\}\}/g;
-    let options = text.match(regex);
-    if (options && options.length > 0) {
-        let option = options[0];
-        if (option) {
-            result = option.substring(2, option.length - 2)
-        }
+  let result = "";
+  let regex = /\{\{(.+?)\}\}/g;
+  let options = text.match(regex);
+  if (options && options.length > 0) {
+    let option = options[0];
+    if (option) {
+      result = option.substring(2, option.length - 2);
     }
-    
-    return result
+  }
+
+  return result;
 }
 
+// 获取get请求所有参数
+function getQueryParams() {
+  const url = request.url;
+  if (url.indexOf("?") !== -1) {
+    let o = {};
+    url
+      .substr(url.indexOf("?") + 1)
+      .split("&")
+      .filter(i => i.length > 0)
+      .forEach(i => {
+        let s = i.split("=");
+        o[s[0]] = s[1];
+      });
+    return o;
+  }
+}
 
-// ------ 导入RSA ------
-if(!pm.globals.has("forgeJS")){
-    pm.sendRequest("https://raw.githubusercontent.com/loveiset/RSAForPostman/master/forge.js", (err, res) => {
-    if (!err) {
-        pm.globals.set("forgeJS", res.text())
+// 获取form-data请求数据
+function getFormDataParams() {
+  if (typeof request.data === "string") {
+    return JSON.parse(request.data);
+  } else {
+    return request.data;
+  }
+}
+
+// ------------ 导入RSA ------------
+if (!pm.globals.has("forgeJS")) {
+  pm.sendRequest(
+    "https://raw.githubusercontent.com/iimondo/Postman-encryption/master/forge.js",
+    (err, res) => {
+      if (!err) {
+        pm.globals.set("forgeJS", res.text());
+      }
     }
-})}
+  );
+}
 
 eval(postman.getGlobalVariable("forgeJS"));
 
-
 // ------------ AES 加密 ------------
-function aesEncrypt(content){
-    //console.log('AES: ' + content);
-    const key = CryptoJS.enc.Utf8.parse("Y5MUIOM7BUWI7BQR");
-    const iv = CryptoJS.enc.Utf8.parse('S41AXIPFRFVJL73Z');
-    const encrypted = CryptoJS.AES.encrypt(content, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
-    return encrypted.toString();
-}
+function aesEncrypt(source, key, iv) {
+  if (!key) {
+    throw new Error("没有在初始化配置中找到AES Key");
+  }
 
+  if (!iv) {
+    throw new Error("没有在初始化配置中找到AES iv");
+  }
+
+  const encrypted = CryptoJS.AES.encrypt(source, key, {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+  });
+
+  return encrypted.toString();
+}
 
 // ------------ RSA 加密 ------------
-function rsaEncrypt(content){
-    const pubKey = pm.environment.get("RSA_Public_Key");
-    if(pubKey){
-        const publicKey = forge.pki.publicKeyFromPem(pubKey);
-        const encryptedText = forge.util.encode64(
-            publicKey.encrypt(content, 'RSAES-PKCS1-V1_5', {
-              md: forge.md.sha1.create(),
-              mgf: forge.mgf.mgf1.create(forge.md.sha1.create())
-        }));
-        
-        return encryptedText;
+function rsaEncrypt(source, pubKey) {
+  if (!pubKey) {
+    throw new Error("未初始化RSA加密公钥，请配置环境变量");
+  }
+
+  const publicKey = forge.pki.publicKeyFromPem(pubKey);
+  const encryptedText = forge.util.encode64(
+    publicKey.encrypt(source, "RSAES-PKCS1-V1_5", {
+      md: forge.md.sha1.create(),
+      mgf: forge.mgf.mgf1.create(forge.md.sha1.create())
+    })
+  );
+
+  return encryptedText;
+}
+
+// ------------ 初始化配置 ------------
+// 所有请求参数
+let requestParams = Object.assign(
+  getFormDataParams(),
+  request.headers,
+  getQueryParams()
+);
+
+// 开始
+function start(configs) {
+  configs = Object.assign({ splitter: "$", pubKeyName: "pubKey" }, configs);
+  if (!pm.environment.has("configs")) {
+    pm.environment.set("configs", JSON.stringify(configs));
+  }
+
+  let localStore = {};
+  if (pm.environment.has("localStore")) {
+    localStore = JSON.parse(pm.environment.get("localStore"));
+  }
+
+  Object.keys(requestParams).map(key => {
+    let value = requestParams[key] + "";
+    if (value.indexOf("{{") !== -1) {
+      let content = getBracketStr(value);
+      if (content.indexOf(`aes${configs.splitter}`) !== -1) {
+        let c = content.split(`aes${configs.splitter}`)[1];
+        let encryptedContent = pm.environment.get(c); // 变量内容
+        encryptedContent = encryptedContent ? encryptedContent : c;
+        pm.environment.set(
+          content,
+          aesEncrypt(encryptedContent, configs.key, configs.iv)
+        );
+        localStore[content] = aesEncrypt(
+          encryptedContent,
+          configs.key,
+          configs.iv
+        );
+      } else if (content.indexOf(`rsa${configs.splitter}`) !== -1) {
+        let c = content.split(`rsa${configs.splitter}`)[1];
+        let encryptedContent = pm.environment.get(c); // 变量内容
+        encryptedContent = encryptedContent ? encryptedContent : c;
+        pm.environment.set(
+          content,
+          rsaEncrypt(encryptedContent, pm.environment.get(configs.pubKeyName))
+        );
+        localStore[content] = rsaEncrypt(
+          encryptedContent,
+          pm.environment.get(configs.pubKeyName)
+        );
+      } else if (content.indexOf(`md5${configs.splitter}`) !== -1) {
+        let c = content.split(`md5${configs.splitter}`)[1];
+        let encryptedContent = pm.environment.get(c); // 变量内容
+        encryptedContent = encryptedContent ? encryptedContent : c;
+        pm.environment.set(content, CryptoJS.MD5(encryptedContent).toString());
+        localStore[content] = CryptoJS.MD5(encryptedContent).toString();
+      }
     }
-    
-    throw new Error('未初始化RSA加密公钥，请配置RSA_Public_Key环境变量');
+  });
+
+  // 环境变量的Key和value
+  pm.environment.set("localStore", JSON.stringify(localStore));
 }
-
-
-// ------ 存储所有未加密环境变量 ------
-if(!pm.environment.has('localStore')){
-    pm.environment.set('localStore', '{}');
-}
-let localStore = JSON.parse(pm.environment.get('localStore'));
-// 获取当前请求中的加密变量
-let requestData; 
-if((typeof request.data) === 'string'){
-    requestData = JSON.parse(request.data)
-} else {
-    requestData = request.data;
-}
-
-requestData = Object.assign(requestData, request.headers);
-Object.keys(requestData).map(key => {
-    let value = requestData[key] + ''; // 内容
-    if (value.indexOf('{{') !== -1) { // 是否为变量
-        let content = getBracketStr(value);
-        // 判断是否加密
-        if (content.indexOf('aes$') !== -1) {
-            let c = content.split('aes$')[1];
-            let encryptedContent = pm.environment.get(c); // 变量内容
-            encryptedContent = encryptedContent ? encryptedContent : c;
-            pm.environment.set(content, aesEncrypt(encryptedContent));
-            localStore[content] = aesEncrypt(encryptedContent);
-        } else if (content.indexOf('rsa$') !== -1) {
-            let c = content.split('rsa$')[1];
-            let encryptedContent = pm.environment.get(c); // 变量内容
-            encryptedContent = encryptedContent ? encryptedContent : c;
-            pm.environment.set(content, rsaEncrypt(encryptedContent));
-            localStore[content] = rsaEncrypt(encryptedContent);
-        } else if(content.indexOf('md5$') !== -1){
-            let c = content.split('md5$')[1];
-            let encryptedContent = pm.environment.get(c); // 变量内容
-            encryptedContent = encryptedContent ? encryptedContent : c;
-            pm.environment.set(content, CryptoJS.MD5(encryptedContent).toString());
-            localStore[content] = CryptoJS.MD5(encryptedContent).toString();
-        }
-    }
-});
-
-pm.environment.set('localStore', JSON.stringify(localStore));
