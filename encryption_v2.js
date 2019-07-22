07eca1ca40d958296af939dced7c399740685d31
mondo <helloxiangsheng@163.com>
2019-07-22 19:23:54 +0800

    .

--- encryption_v2.js
+++ encryption_v2.js
@@ -1,7 +1,8 @@
 (function (env) {
     // 内置工具类
     class _Util {
-        constructor(env) {
+        constructor(env, log) {
+            this.isLog = log;
             this.JSON = env.JSON;
             this.request = env.request;
             this.console = env.console;
@@ -9,7 +10,7 @@
 
         // 日志
         log(message) {
-            this.console.log(message);
+            this.isLog && this.console.log(message);
         }
 
         // 在 '{{data}}' 符号中找到内容
@@ -24,13 +25,13 @@
             }
 
             this.log(`raw data : ${rawData} , valid data : ${validData}`);
-            this.log('------------ findValidData ------------')
+            this.log('------------ findValidData ------------');
             return validData;
         }
 
         // FORM-DATA 参数转为对象返回
         findFormDataParameter(parameters) {
-            return parameters === "string" ? JSON.parse(parameters) : parameters;
+            return (typeof parameters === "string") ? this.JSON.parse(parameters) : parameters;
         }
 
         // URL 参数转为对象返回
@@ -41,12 +42,11 @@
                     .forEach(index => {
                         let result = index.split("=");
                         parameter[result[0]] = result[1];
-
                         this.log(`name : ${result[0]} , value : ${result[1]}`);
                     });
             }
 
-            this.log('------------ findUrlParameter ------------');
+            this.log('------------ URL上的参数 ------------');
             return parameter;
         }
 
@@ -56,52 +56,110 @@
         }
     }
 
+    /**
+     * 加密抽象类
+     */
+    class AbsEncrypt {
 
-    // 对外开放对象
-    class XEncryption {
-        // 注册配置
-        static register() {
-
+        constructor(name, splitter) {
+            this.name = name;
+            this.localStore = {};
+            this.splitter = splitter;
         }
 
-        // AES/CBC/Pkcs7 加密
-        AES(source, key, iv) {
-            if (!key) {
-                throw new Error("没有在初始化配置中找到AES Key");
-            }
-
-            if (!iv) {
-                throw new Error("没有在初始化配置中找到AES iv");
-            }
-
-            return CryptoJS.AES.encrypt(source, key, {
-                iv: iv,
-                mode: CryptoJS.mode.CBC,
-                padding: CryptoJS.pad.Pkcs7
-            }).toString();
+        getEncryptedContent(raw) {
+            let raw = raw.split(`${this.name + this.splitter}`)[1];
+            let encryptedContent = env.pm.environment.get(raw);
+            encryptedContent = encryptedContent ? encryptedContent : raw;
+            return encryptedContent;
         }
 
-        // RSA/PKCS1 加密
-        RSA(source, pubKey) {
-            if (!pubKey) {
-                throw new Error("未初始化RSA加密公钥，请配置环境变量");
-            }
+        encrypt(raw) {
+            let encryptValue = this.getEncryptedContent(raw);
+            this.localStore[content] = this.overrder(encryptValue);
+            pm.environment.set("localStore", JSON.stringify(this.localStore));
+        }
 
-            const publicKey = forge.pki.publicKeyFromPem(pubKey);
-            const encryptedText = forge.util.encode64(
-                publicKey.encrypt(source, "RSAES-PKCS1-V1_5", {
-                    md: forge.md.sha1.create(),
-                    mgf: forge.mgf.mgf1.create(forge.md.sha1.create())
-                })
-            );
-
-            return encryptedText;
+        overrder(raw) {
+            // 子类重写此方法，实现具体加密
         }
     }
 
 
-    // 扩展postman方法
-    env.register = XEncryption.register;
-})(this)
+    /**
+     * MD5 加密
+     */
+    class MD5 extends AbsEncrypt {
 
-this.aid.util.findRequestParameter()
+        constructor(splitter) {
+            super('md5', splitter);
+        }
+
+        overrder(data) {
+            return CryptoJS.MD5(data).toString();
+        }
+    }
+
+
+    /**
+     * AES加密
+     */
+    class AES extends AbsEncrypt {
+
+        /**
+         * @param {*} splitter // 加密分割符, 分割加密方式和加密内容
+         * @param {*} key      // AES 密钥
+         * @param {*} iv       // AES 偏移量
+         */
+        constructor(splitter, key, iv) {
+            super('aes', splitter);
+            this.iv = iv;
+            this.key = key;
+
+            if (!this.key) { throw new Error("没有在初始化配置中找到AES Key"); }
+            if (!this.iv) { throw new Error("没有在初始化配置中找到AES iv"); }
+        }
+
+        overrder(data) {
+            return CryptoJS.AES.encrypt(data, this.key, {
+                iv: this.iv,
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7
+            });
+        }
+    }
+
+
+    // 初始化
+    function register(configs = {
+        log: false,         // 日志开关
+        splitter: "$",      // 加密分割符，分割加密方式和加密内容
+        key: '',            // AES 密钥
+        iv: '',             // AES 偏移量
+        privateKey: ''      // RSA 私钥
+    }, encryptMethods) {
+        const util = new _Util(env, configs.log);
+        const requestParameters = util.findRequestParameter();
+
+        // 支持的加密方式
+        let supportMethods = [new AES(configs.splitter, configs.key, configs.iv), new MD5(configs.splitter)];
+
+        // 遍历所有请求参数，执行加密
+        Object.keys(requestParameters).map(key => {
+            let value = requestParameters[key].toString();
+            if (value.indexOf("{{") !== -1) {
+                let validData = util.findValidData(value);
+                supportMethods.forEach(method => method.encrypt(validData));
+            }
+        });
+    }
+
+
+    // 导出方法, 对象
+    env.register = register;
+    env.AbsEncrypt = AbsEncrypt;
+})(this);
+
+
+
+this.register({ log: true });
