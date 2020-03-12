c25b5351872097e478dbb54b4b2f5fb012ae9bd6
mondo <helloxiangsheng@163.com>
2020-03-12 18:02:27 +0800

    .

--- encryp/clearTrace.js
+++
@@ -1,9 +0,0 @@
-(function clera(){
-    if(!pm.environment.has('HistoryTrace')){
-        pm.environment.set('HistoryTrace', '{}');
-    }
-
-    let localStore = JSON.parse(pm.environment.get('HistoryTrace'));
-    Object.keys(localStore).map(key => pm.environment.unset(key));
-    pm.environment.unset('HistoryTrace');
-})();
